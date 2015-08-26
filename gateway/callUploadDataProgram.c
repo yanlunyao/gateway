65e4b0a3eb50347178e5a347883169995c286dd3
yanly <413899391@qq.com>
2015-08-26 20:10:00 +0800

    多进程执行shell命令；

--- gateway/callUploadDataProgram.c
+++ gateway/callUploadDataProgram.c
@@ -10,10 +10,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "cgic.h"
 #include "cJSON.h"
 
-static void api_response(int res, const char *datatype, const char *cmdstring);
+static void api_response(int res, int datatype, const char *cmdstring);
 /*
  * api:	http://192.168.1.227/cgi-bin/rest/network/CallUploadDataProgram.cgi?datatype=1
  * */
@@ -21,30 +22,43 @@ int cgiMain()
 {
 	cgiFormResultType cgi_re;
 	int res=0;
-	char datatype[4]={0};
-	char cmdstring[100] = "/gl/bin/BaseDataUpload";
+	int datatype;
+	char cmdstring[100] = {0};
 
 	cgiHeaderContentType("application/json"); //MIME
-	cgi_re = cgiFormString("datatype", datatype, sizeof(datatype)+1);
+	cgi_re = cgiFormInteger("datatype", &datatype, -1);
 
-	if(cgi_re == cgiFormSuccess) {
-		sprintf(cmdstring,"%s %s", "/gl/bin/BaseDataUpload", datatype);
+	if(datatype ==-1)
+		snprintf(cmdstring, sizeof(cmdstring), "%s", "/gl/bin/BaseDataUpload");
+	else
+		snprintf(cmdstring, sizeof(cmdstring), "%s %d", "/gl/bin/BaseDataUpload", datatype);
+
+    fflush(stdout);
+    pid_t	pid;
+	if ((pid = fork()) > 0) {						//parent process
 	}
-	res = system(cmdstring);
-	//调用程序
-
+	else
+	{												//child process
+		//重定向输入输出流
+		freopen("/dev/null", "r", stdin);
+		freopen("/dev/null", "w", stdout);
+		freopen("/dev/null", "w", stderr);
+		system(cmdstring);
+		exit(0);
+	}
+	res =0;
 	api_response(res, datatype, cmdstring);
 	return 0;
 }
 
-static void api_response(int res, const char *datatype, const char *cmdstring)
+static void api_response(int res, int datatype, const char *cmdstring)
 {
 	cJSON *root;
 	char *json_out;
 
 	root=cJSON_CreateObject();
 	cJSON_AddNumberToObject(root, "status", res);
-	cJSON_AddStringToObject(root, "datatype", datatype);
+	cJSON_AddNumberToObject(root, "datatype", datatype);
 	cJSON_AddStringToObject(root, "cmdstring", cmdstring);
 	json_out=cJSON_Print(root);
 	cJSON_Delete(root);
