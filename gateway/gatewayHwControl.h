34db6148cbb537e6a8167233ffda8486d31102d4
yanly <413899391@qq.com>
2015-09-02 10:13:52 +0800

    网络断线，关cloud灯的硬件映射成米尔板。

--- gateway/gatewayHwControl.h
+++ gateway/gatewayHwControl.h
@@ -13,5 +13,8 @@
 #define SETIO_L			"echo 0 > /sys/class/gpio/gpio47/value"
 #define SETIO_H			"echo 1 > /sys/class/gpio/gpio47/value"
 
+//cloud led
+#define SET_DARK_CLOUD 	"echo 0 > /sys/class/leds/cloud_led/brightness"
+#define SET_LIGHT_CLOUD "echo 1 > /sys/class/leds/cloud_led/brightness"
 
 #endif /* GATEWAY_GATEWAYHWCONTROL_H_ */
